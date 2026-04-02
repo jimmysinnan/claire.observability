e6865ed761f427b6433abf47a81d5797916b3338
jimmysinnan <93935110+jimmysinnan@users.noreply.github.com>
2026-04-02 21:29:20 +0200

    Implement telemetry configuration with OpenTelemetry
    Replaced logging configuration with telemetry setup using OpenTelemetry.

--- app/core/telemetry.py
+++ app/core/telemetry.py
@@ -1,13 +1,16 @@
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+_INITIALIZED = False
 
 
-def configure_logging() -> None:
-    root = logging.getLogger()
-    if root.handlers:
+def configure_telemetry() -> None:
+    global _INITIALIZED
+    if _INITIALIZED:
         return
-
-    handler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter("%(asctime)s %(levelname)s %(name)s %(message)s")
-    handler.setFormatter(formatter)
-
-    root.setLevel(logging.INFO)
-    root.addHandler(handler)
+    provider = TracerProvider()
+    provider.add_span_processor(BatchSpanProcessor(OTLPSpanExporter(insecure=True)))
+    trace.set_tracer_provider(provider)
+    _INITIALIZED = True
